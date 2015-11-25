a4af3b93cba1c30fb4d2fec691fd652ba6f8ba10
michael_wojcik <michael_wojcik@3024393.no-reply.drupal.org>
2015-11-25 16:50:40 -0500

    Added in "Research Interests" and "Selected Projects" as fields on the Open Eggheads Personal Data form. Created corresponding CTools content types and templates to display this data within the Panels ecosystem.

---
+++ sites/all/modules/custom/open_eggheads_admin_interface/open_eggheads_ctools_plugins/plugins/content_types/templates/open-eggheads-research-interests.tpl.php
@@ -0,0 +1,15 @@
+<div class="open-eggheads-research-interests">
+  <<?php print $research_interests_list_type; ?>
+    <?php if ($research_interests_list_type == 'ul'): ?>
+      style="list-style-type:<?php print $research_interests_list_style_ul; ?>"
+    <?php elseif ($research_interests_list_type == 'ol'): ?>
+      type="<?php print $research_interests_list_style_ol; ?>"
+    <?php endif; ?>
+  >
+    <?php foreach ($research_interests as $research_interest): // Print all research interests provided by user. ?>
+      <?php if ($research_interest): // Don't show research interest fields that were left blank. ?>
+        <li><?php print $research_interest; ?></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </<?php print $research_interests_list_type; ?>>
+</div>
---
+++ sites/all/modules/custom/open_eggheads_admin_interface/open_eggheads_ctools_plugins/plugins/content_types/templates/open-eggheads-selected-projects.tpl.php
@@ -0,0 +1,15 @@
+<div class="open-eggheads-selected-projects">
+  <<?php print $selected_projects_list_type; ?>
+    <?php if ($selected_projects_list_type == 'ul'): ?>
+      style="list-style-type:<?php print $selected_projects_list_style_ul; ?>"
+    <?php elseif ($selected_projects_list_type == 'ol'): ?>
+      type="<?php print $selected_projects_list_style_ol; ?>"
+    <?php endif; ?>
+  >
+    <?php foreach ($selected_projects as $selected_project): // Print all selected projects provided by user. ?>
+      <?php if ($selected_project): // Don't show selected project fields that were left blank. ?>
+        <li><?php print $selected_project; ?></li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  </<?php print $selected_projects_list_type; ?>>
+</div>
