8502911128db40db12d297ffd667ae71f4ccc1ef
michael_wojcik <michael_wojcik@3024393.no-reply.drupal.org>
2015-11-30 15:33:42 -0500

    Added new custom module Open Eggheads Publications, to better implement the contributed module Biblio for handling the user's publications.

---
+++ sites/all/modules/custom/open_eggheads_admin_interface/open_eggheads_publications/js/open_eggheads_publications_biblio_node_form.js
@@ -0,0 +1,6 @@
+(function($) {
+  $(document).ready(function(){
+    $('#edit-field-biblio-pdf-und-0-browse-button').addClass('btn btn-default btn-primary');
+    $('#edit-field-biblio-pdf').detach().prependTo('.radix-layouts-sidebar');
+  });
+})(jQuery);
