9b026ab169341609e48152cdadca84722fd88193
michael-wojcik <michael-wojcik@users.noreply.github.com>
2015-12-07 02:41:54 -0500

    Added an Open Eggheads 'Frequently Asked Questions' page. FAQ page will link to JavaScript tours of the site's functionality. Also created a persistent Help button that will be visible to all logged-in admin users, granting them quick access to the new FAQ page.

---
+++ sites/all/modules/custom/open_eggheads_admin_interface/js/open_eggheads_admin_interface.help_button.js
@@ -0,0 +1,25 @@
+(function($) {
+  // Define Help button in HTML.
+  var $helpButton = $('<button id="open-eggheads-help-button" class="btn btn-sm btn-success"><i class="fa fa-question-circle fa-lg"></i> <strong>Help</strong></button>');
+  
+  // Append Help button to Admin Menu bar and position it below the bar.
+  function openEggheadsAdminInterfaceHelpButtonInit() {
+    $('#admin-menu').append($helpButton);
+    $($helpButton).click(function() {
+      // Redirect to the FAQ page.
+      window.location.href = '/admin/open-eggheads/faq';
+    });
+  }
+  
+  // Create Help button on page according to Drupal.behaviors standards.
+  Drupal.behaviors.openEggheadsAdminInterfaceHelpButton = {
+    // Called when new content is added to the DOM, e.g. via an AJAX request
+    // 'context' is the (newly) relevant portion of the DOM
+    attach: function(context, settings) {
+      openEggheadsAdminInterfaceHelpButtonInit()
+    },
+    detach: function(context, settings) {
+      openEggheadsAdminInterfaceHelpButtonInit()
+    }
+  };
+})(jQuery);
